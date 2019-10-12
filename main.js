711645d9907313b335b6de81ab2464e39c8cd9bc
Jasper Khonesavanh <jasperkhonesavanh@gmail.com>
2019-10-11 17:46:10 -0700

    More Progress

--- main.js
+++ main.js
@@ -9,22 +9,30 @@ var template = `
 </div>
 `
 
-// const html = items.results.map(item => {
-//     return `
-//     <div class="product">
-//     <img src="" alt="">
-//     <h3 class="itemname">${item.results.currency_code}</h3>
-//     <div class="info">
-//         <p class="maker">Maker</p>
-//         <p class="price">Price</p>
-//     </div>
-//     </div>
-//     `
-// }).join("")
+const html = items.results.map(item => {
+    if (item.currency_code === "USD") {
+        item.currency_code = "$"
+    } else if (item.currency_code == "EUR") {
+        item.currency_code = "&euro;"
+    } else if (item.currency_code == "GBP") {
+        item.currency_code = "&pound"
+    }
+    return `
+    <div class="product">
+    <img src="${item.Images[0].url_fullxfull}" alt="">
+    <p class="itemname">${item.title}</p>
+    <div class="info">
+        <p class="stock">${item.quantity} left in stock</p>
+        <div id="rating">
+            <div class="rate"><p>(${item.views})</p></div>
+        </div>
+        <p class="price">${item.currency_code}${item.price}</p>
+    </div>
+    </div>
+    `
+}).join("")
 
-// console.log(html)
-
-// document.querySelector("#products").innerHTML = html
+document.querySelector("#products").innerHTML = html
 
 // var example = items.map(item => {
 //     return `${item.results.title}`
@@ -32,8 +40,4 @@ var template = `
 
 // console.log(items.results[0].currency_code)
 
-const example = items.map(item => {
-    return `${item.results}`
-})
-
-console.log(example)
+console.log(items.results[0])
